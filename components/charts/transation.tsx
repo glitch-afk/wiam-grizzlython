2fe11c8cecfdb182296d0d4b04088027451ef28e
glitch-afk <rahul004@proton.me>
2023-02-23 00:21:38 +0530

    :sparkles: added transaction table and a custom select component to change chart data

---
+++ components/charts/transation.tsx
@@ -0,0 +1,67 @@
+import React from "react"
+import {
+  monthlyComparison,
+  weeklyComparison,
+  yearlyComparison,
+} from "@/data/index"
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts"
+
+const TransactionChart = ({ chartData }: { chartData: any }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <AreaChart
+        width={500}
+        height={400}
+        data={chartData}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#9747FF" stopOpacity={0.4} />
+            <stop offset="95%" stopColor="#9747FF" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <CartesianGrid
+          strokeDasharray="10 5"
+          vertical={false}
+          stroke="#464C57"
+        />
+        <XAxis
+          dataKey="name"
+          tick={{ stroke: "#464C57" }}
+          axisLine={false}
+          tickLine={false}
+          fontSize={14}
+        />
+        <YAxis
+          tick={{ stroke: "#464C57" }}
+          axisLine={false}
+          tickLine={false}
+          fontSize={14}
+        />
+        <Area
+          type="monotone"
+          dataKey="btc"
+          stroke="#9747FF"
+          fill="url(#colorUv)"
+        />
+        <Tooltip />
+      </AreaChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default TransactionChart
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,17 +1,50 @@
-import React from "react"
+import React, { useState } from "react"
 import { OverViewCardsData } from "@/data"
+import {
+  monthlyComparison,
+  weeklyComparison,
+  yearlyComparison,
+} from "@/data/index"
 
 import OverviewChart from "@/components/charts/overview"
+import TransactionChart from "@/components/charts/transation"
 import DashboardLayout from "@/components/dashboard/_layout"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 const strokeColors = ["#9747FF", "#F92672", "#4DA765", "#FF8947"]
 
 const Dashboard = () => {
+  const [transactionChartData, setTransactionChartData] =
+    useState(yearlyComparison)
+
+  const handleOnChange = (value: string) => {
+    switch (value) {
+      case "Week":
+        setTransactionChartData(weeklyComparison)
+        break
+      case "Month":
+        setTransactionChartData(monthlyComparison)
+        break
+      case "Year":
+        setTransactionChartData(yearlyComparison)
+        break
+      default:
+        setTransactionChartData(yearlyComparison)
+        break
+    }
+  }
+
   return (
     <DashboardLayout>
-      <div className="bg-brand-600 border-brand-400 w-full rounded-lg border p-8">
+      <div className="bg-brand-600 border-brand-400 w-full rounded-lg border p-4 md:p-8">
         {/* overview cards */}
-        <div className="3xl:grid-cols-4 grid w-full grid-cols-1 gap-8 md:grid-cols-2">
+        <div className="3xl:grid-cols-4 grid w-full grid-cols-1 gap-4 md:grid-cols-2 md:gap-8">
           {OverViewCardsData.map(
             ({ title, dataDisplay, percentChange }, idx) => (
               <div className="bg-brand-500 rounded-md p-4" key={idx}>
@@ -26,6 +59,24 @@ const Dashboard = () => {
               </div>
             )
           )}
+        </div>
+        {/* volume chart */}
+        <div>
+          <div className="mt-16">
+            <Select onValueChange={handleOnChange}>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Yearly Data" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="Week">Weekly Data</SelectItem>
+                <SelectItem value="Month">Monthly Data</SelectItem>
+                <SelectItem value="Year">Yearly Data</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="3xl:h-[496px] 4xl:h-[580px] -mx-6 mt-5 h-56 sm:mt-8 md:h-96 lg:h-[416px] xl:h-[479px]">
+            <TransactionChart chartData={transactionChartData} />
+          </div>
         </div>
       </div>
     </DashboardLayout>
