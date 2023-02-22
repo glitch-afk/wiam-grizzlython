60fe373127a5be6529bc565c198e7ad4d91bb36b
glitch-afk <rahul004@proton.me>
2023-02-22 20:53:12 +0530

    :beers: managed loading state in user component

--- components/user.tsx
+++ components/user.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import Link from "next/link"
 import { NavItem } from "@/types"
-import { Item } from "@radix-ui/react-dropdown-menu"
 import { signOut, useSession } from "next-auth/react"
 
 import { cn } from "@/lib/utils"
@@ -14,6 +13,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Icons } from "./icons"
+
+const Loader = () => {
+  return <Icons.loader className="h-auto w-5 animate-spin text-white" />
+}
 
 const User = ({
   userClasses,
@@ -22,43 +26,54 @@ const User = ({
   userClasses?: string
   navItems?: NavItem[]
 }) => {
-  const { data: session } = useSession()
+  const { data: session, status } = useSession()
+  const isLoading = status === "loading"
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger
-        className={cn(
-          "text-brand-50 hover:outline-brand-400 flex min-w-fit cursor-pointer items-center p-1 hover:text-white hover:outline-1 focus:outline-none lg:p-2",
-          userClasses
-        )}
-      >
-        <Avatar className="h-auto w-6 sm:w-8">
-          <AvatarImage src={session?.user.image as string} />
-          <AvatarFallback>W</AvatarFallback>
-        </Avatar>
-        <span className="text-sm md:hidden lg:flex">{session?.user.email}</span>
-        {/* </div> */}
-      </DropdownMenuTrigger>
-      <DropdownMenuContent side="bottom" align="end">
-        <DropdownMenuLabel>Signed In as</DropdownMenuLabel>
-        <DropdownMenuItem>{session?.user.name}</DropdownMenuItem>
-        <DropdownMenuSeparator />
-        {navItems?.length ? (
-          <div className="md:hidden">
-            {navItems.map((item, idx) => (
-              <Link
-                href={item.disabled ? "#" : (item.href as string)}
-                key={idx}
-              >
-                <DropdownMenuItem>{item.title}</DropdownMenuItem>
-              </Link>
-            ))}
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            className={cn(
+              "text-brand-50 hover:outline-brand-400 flex min-w-fit cursor-pointer items-center p-1 hover:text-white hover:outline-1 focus:outline-none lg:p-2",
+              userClasses
+            )}
+          >
+            <Avatar className="h-auto w-6 sm:w-8">
+              <AvatarImage src={session?.user.image as string} />
+              <AvatarFallback>W</AvatarFallback>
+            </Avatar>
+            <span className="text-sm md:hidden lg:flex">
+              {session?.user.email}
+            </span>
+            {/* </div> */}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent side="bottom" align="end">
+            <DropdownMenuLabel>Signed In as</DropdownMenuLabel>
+            <DropdownMenuItem>{session?.user.name}</DropdownMenuItem>
             <DropdownMenuSeparator />
-          </div>
-        ) : null}
+            {navItems?.length ? (
+              <div className="md:hidden">
+                {navItems.map((item, idx) => (
+                  <Link
+                    href={item.disabled ? "#" : (item.href as string)}
+                    key={idx}
+                  >
+                    <DropdownMenuItem>{item.title}</DropdownMenuItem>
+                  </Link>
+                ))}
+                <DropdownMenuSeparator />
+              </div>
+            ) : null}
 
-        <DropdownMenuItem onClick={() => signOut()}>Log Out</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+            <DropdownMenuItem onClick={() => signOut()}>
+              Log Out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
+    </>
   )
 }
 
