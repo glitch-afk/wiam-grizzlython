cec9015d07e280b85e041d883d78fca8b153f5dd
glitch-afk <rahul004@proton.me>
2023-03-13 16:40:16 +0530

    :fire: !zod & cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,3 @@
-// @ts-check
-
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds.
- */
-!process.env.SKIP_ENV_VALIDATION && (await import("./env.mjs"))
-
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
